b59b0caaec9c813eef64a68a6bba059d7b5f054c
alison <adevpr11@gmail.com>
2023-08-31 17:33:00 -0300

    Car Section

--- src/routes/Home/index.tsx
+++ src/routes/Home/index.tsx
@@ -2,6 +2,17 @@ import Header from '../../components/Header';
 import './styles.css';
 
 export default function Home() {
-    return (
-        <Header></Header>);
+    return (<>
+        <Header></Header>
+        <main>
+            <section id='dstop-section-cars'>
+                <h2>Venha nos visitar</h2>
+                <div className='dstop-card-container'>
+
+                </div>
+
+            </section>
+        </main>
+    </>
+    );
 }
